8b80b20254c42e8c7f8872065a5e00a48725f11a
Jayesh <jayeshharda@gmail.com>
2023-11-06 00:17:51 +0530

    Revert "render deployment"
    This reverts commit b893f830ab6fea88032d1fabe4bc87d8aa950277.

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ ruby "3.2.2"
 gem 'bootstrap', '~> 5.3', '>= 5.3.1'
 gem 'jquery-rails'
 gem 'devise', '~> 4.9', '>= 4.9.2'
-gem 'pg'
 
 gem "byebug"
 
@@ -17,7 +16,7 @@ gem "rails", "~> 7.0.8"
 gem "sprockets-rails"
 
 # Use sqlite3 as the database for Active Record
-# gem "sqlite3", "~> 1.4"
+gem "sqlite3", "~> 1.4"
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_05_182749) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
+ActiveRecord::Schema[7.0].define(version: 2023_11_04_184014) do
   create_table "projects", force: :cascade do |t|
     t.string "projectname"
     t.datetime "created_at", null: false
