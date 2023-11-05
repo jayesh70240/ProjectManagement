b893f830ab6fea88032d1fabe4bc87d8aa950277
Jayesh <jayeshharda@gmail.com>
2023-11-06 00:17:51 +0530

    render deployment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_04_184014) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_05_182749) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: :cascade do |t|
     t.string "projectname"
     t.datetime "created_at", null: false
